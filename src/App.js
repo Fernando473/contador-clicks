e5ea270909a917dae798bf7a1c0c968154403e6a
fernando473 <jhon.sangopanta@epn.edu.ec>
2022-09-21 14:51:02 -0500

    Footer fixed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import freeCodeCampLogo from "../src/imagenes/freeCodeCampLogo.png"
 import Boton from './componentes/Boton';
 import Contador from './componentes/Contador';
 import { useState } from 'react'
-import { FaBeer  } from "react-icons/fa"
+import { AiOutlineInstagram, AiOutlineGithub , AiOutlineTwitter, AiOutlineFacebook }  from "react-icons/ai"
 
 function App() {
 
@@ -40,10 +40,15 @@ function App() {
         </Boton>
       </div>
       <footer> 
-        <FaBeer ></FaBeer >
         <span>Fernando Sangopanta</span> 
-        <span>Software Engennering Student </span>
-        <FaBeer ></FaBeer >
+        <span>Software Engennering Student at EPN </span>
+        <div className='iconos'>
+          <a href='https://www.facebook.com/fernando.sangopanta.714' target='blank' ><AiOutlineFacebook className='icon'></AiOutlineFacebook></a>
+          <a href='https://twitter.com/Fernand95410668' target='blank'><AiOutlineTwitter className='icon'></AiOutlineTwitter></a>
+          <a href='https://github.com/Fernando473' target='blank'><AiOutlineGithub className='icon'></AiOutlineGithub></a>
+          <a href='https://www.instagram.com/jhonfer1999/' target='blank'><AiOutlineInstagram className='icon'></AiOutlineInstagram></a>
+        </div>
+
       </footer>
     </div>
 
