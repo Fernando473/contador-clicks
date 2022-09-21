0aa4b07156b86a28fad07ba13b472ca40a9595bf
fernando473 <jhon.sangopanta@epn.edu.ec>
2022-09-21 14:58:57 -0500

    Refactorized

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import freeCodeCampLogo from "../src/imagenes/freeCodeCampLogo.png"
 import Boton from './componentes/Boton';
 import Contador from './componentes/Contador';
 import { useState } from 'react'
-import { AiOutlineInstagram, AiOutlineGithub , AiOutlineTwitter, AiOutlineFacebook }  from "react-icons/ai"
+import Footer from './componentes/Footer';
+
 
 function App() {
 
@@ -38,18 +39,8 @@ function App() {
           esBotonDeClick={false}
           manejarClick={reiniciarContador} >
         </Boton>
+        <Footer></Footer> 
       </div>
-      <footer> 
-        <span>Fernando Sangopanta</span> 
-        <span>Software Engennering Student at EPN </span>
-        <div className='iconos'>
-          <a href='https://www.facebook.com/fernando.sangopanta.714' target='blank' ><AiOutlineFacebook className='icon'></AiOutlineFacebook></a>
-          <a href='https://twitter.com/Fernand95410668' target='blank'><AiOutlineTwitter className='icon'></AiOutlineTwitter></a>
-          <a href='https://github.com/Fernando473' target='blank'><AiOutlineGithub className='icon'></AiOutlineGithub></a>
-          <a href='https://www.instagram.com/jhonfer1999/' target='blank'><AiOutlineInstagram className='icon'></AiOutlineInstagram></a>
-        </div>
-
-      </footer>
     </div>
 
   );
---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { AiOutlineInstagram, AiOutlineGithub , AiOutlineTwitter, AiOutlineFacebook }  from "react-icons/ai"
+import "../estilos/Footer.css"
+
+const Footer = () =>    {
+    return(
+        <footer> 
+            <span>Fernando Sangopanta</span> 
+            <span>Software Engennering Student at EPN </span>
+            <div className='iconos'>
+            <a href='https://www.facebook.com/fernando.sangopanta.714' target='blank' ><AiOutlineFacebook className='icon'></AiOutlineFacebook></a>
+            <a href='https://twitter.com/Fernand95410668' target='blank'><AiOutlineTwitter className='icon'></AiOutlineTwitter></a>
+            <a href='https://github.com/Fernando473' target='blank'><AiOutlineGithub className='icon'></AiOutlineGithub></a>
+            <a href='https://www.instagram.com/jhonfer1999/' target='blank'><AiOutlineInstagram className='icon'></AiOutlineInstagram></a>
+            </div>
+      </footer>
+    )
+}
+
+export default Footer;
